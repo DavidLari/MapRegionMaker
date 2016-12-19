1d13b968b54f22dc93acb3678bb22cbc8b419a79
David Lari <david@davidlari.com>
2016-12-19 13:04:38 -0500

    Initial Commit

---
+++ MapRegionMaker/MapRegionMaker.h
@@ -0,0 +1,19 @@
+//
+//  MapRegionMaker.h
+//  MapRegionMaker
+//
+//  Created by David Lari on 12/19/16.
+//  Copyright © 2016 David Lari Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapRegionMaker.
+FOUNDATION_EXPORT double MapRegionMakerVersionNumber;
+
+//! Project version string for MapRegionMaker.
+FOUNDATION_EXPORT const unsigned char MapRegionMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapRegionMaker/PublicHeader.h>
+
+
